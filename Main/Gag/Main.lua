4625f46818fa7e87042c20463e57f8941b4d5b67
TIMBERWOLVES <masteredge454@gmail.com>
2025-07-27 11:15:48 +0800

    Update Main.lua

--- Main/Gag/Main.lua
+++ Main/Gag/Main.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/bSuBeKZYltdDYd2f/raw", true))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua'))()
+loadstring(game:HttpGet("https://pastefy.app/ieYqHf49/raw"))()
